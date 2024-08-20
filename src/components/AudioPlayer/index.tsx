47c109fe1a7a01eacd323926670d5ed061294db8
Prince Bansal <work.princebansal@gmail.com>
2024-08-20 23:13:18 +0530

    volumne functionality added

--- src/components/AudioPlayer/index.tsx
+++ src/components/AudioPlayer/index.tsx
@@ -1,9 +1,10 @@
-import { FC, useEffect, useRef, useState } from "react";
+import { ChangeEvent, FC, useEffect, useRef, useState } from "react";
 
 import Next from "/next.svg";
 import Play from "/play.svg";
 import Prev from "/prev.svg";
 import Pause from "/pause.svg";
+import Volume from "/volume.svg";
 
 import { IEpisode } from "../../types/Episode";
 import { getTimeFromSeconds } from "../../utils/helper";
@@ -27,6 +28,7 @@ const AudioPlayer: FC<AudioPlayerProps> = ({
   const timerRef = useRef<number>(0);
 
   const [time, setTime] = useState<number>(0);
+  const [volume, setVolume] = useState<number>(50);
 
   useEffect(() => {
     setTime(0);
@@ -68,60 +70,89 @@ const AudioPlayer: FC<AudioPlayerProps> = ({
     };
   }, [audioRef, episode, playNext, time]);
 
+  const onVolumeChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setVolume(+event.target.value);
+    if (audioRef.current) audioRef.current.volume = +event.target.value;
+  };
+
+  const onAudioChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setTime(Math.round(Number(event.target.value)));
+    if (audioRef.current)
+      audioRef.current.currentTime = Number(event.target.value);
+  };
+
+  const togglePlayPause = () => {
+    if (isPlaying) audioRef.current?.pause();
+    else audioRef.current?.play();
+    setIsPlaying(!isPlaying);
+  };
+
   if (!episode) return null;
 
   return (
     <div className="border-2 shadow-black border-t-primary bg-white w-full fixed bottom-0">
-      <div className="flex gap-4 py-2 lg:py-4 w-[90%] lg:w-[80%] m-auto items-center justify-between">
-        <div className="flex items-center gap-2 w-[60%] lg:w-[25%]">
+      <div className="flex gap-x-0 lg:gap-x-10 py-2 lg:py-4 w-[90%] lg:w-[80%] m-auto items-center lg:items-start justify-between">
+        <div className="flex items-center gap-2 w-[50%] lg:w-[25%]">
           <img
             className="w-12 lg:w-16 cursor-pointer rounded-md"
             src={episode?.episode_compressed_image}
+            alt={episode.name}
           />
-          <p className="text-sm line-clamp-2">{episode?.name}</p>
+          <p className="text-xs line-clamp-2">{episode?.name}</p>
         </div>
-        <div className="visible lg:hidden">
-          <img
-            className="w-7 cursor-pointer"
-            onClick={() => {
-              if (isPlaying) audioRef.current?.pause();
-              else audioRef.current?.play();
-              setIsPlaying(!isPlaying);
-            }}
-            src={isPlaying ? Pause : Play}
-          />
-        </div>
-        <div className="hidden lg:block w-[80%] m-auto">
+        <div className="w-[50%] lg:w-[80%] m-auto">
           <input
             value={time}
             max={episode.duration}
-            onChange={(e) => {
-              setTime(Math.round(Number(e.target.value)));
-              if (audioRef.current)
-                audioRef.current.currentTime = Number(e.target.value);
-            }}
+            onChange={onAudioChange}
             type="range"
-            className="w-full accent-primary h-1 outline-none mb-6 bg-gray-100 rounded-lg cursor-pointer"
+            className="w-full hidden mb-3 lg:block accent-primary h-1 outline-none bg-gray-100 rounded-lg cursor-pointer"
           />
 
-          <div className="flex gap-6 justify-center">
-            <img onClick={playPrev} className="w-7 cursor-pointer" src={Prev} />
+          <div className="flex gap-3 lg:gap-6 justify-center">
             <img
-              className="w-7 cursor-pointer"
-              onClick={() => {
-                if (isPlaying) audioRef.current?.pause();
-                else audioRef.current?.play();
-                setIsPlaying(!isPlaying);
-              }}
+              onClick={playPrev}
+              className="lg:w-7 w-5 cursor-pointer"
+              alt="prev-icon"
+              src={Prev}
+            />
+            <img
+              className="lg:w-7 w-5 cursor-pointer"
+              onClick={togglePlayPause}
+              alt="play-pause-icon"
               src={isPlaying ? Pause : Play}
             />
-            <img onClick={playNext} className="w-7 cursor-pointer" src={Next} />
+            <img
+              onClick={playNext}
+              className="lg:w-7 w-5 cursor-pointer"
+              alt="next-icon"
+              src={Next}
+            />
             <audio src={episode.file} ref={audioRef} />
           </div>
         </div>
-        <div className="flex gap-1 justify-end w-[25%]">
-          <span className="w-10">{getTimeFromSeconds(time)}</span> /{" "}
-          <span className="w-10 ">{getTimeFromSeconds(episode?.duration)}</span>
+        <div className="hidden lg:block w-[25%]">
+          <p className="text-xs lg:text-sm text-center">
+            <span className="inline-block w-10">
+              {getTimeFromSeconds(time)}
+            </span>{" "}
+            /{" "}
+            <span className="w-10 inline-block">
+              {getTimeFromSeconds(episode?.duration)}
+            </span>
+          </p>
+          <div className="md:flex items-center hidden gap-2">
+            <img src={Volume} alt="voice-icon" className="w-6" />
+            <input
+              value={volume}
+              max={1}
+              min={0}
+              step="0.01"
+              onChange={onVolumeChange}
+              type="range"
+              className="w-full accent-black h-1 outline-none rounded-lg cursor-pointer"
+            />
+          </div>
         </div>
       </div>
     </div>
